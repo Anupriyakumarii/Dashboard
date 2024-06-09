55a0977e48c28cdb61b13e9a3d3b558cc09a03c9
AnupriyaKumarii <anuyadav20001@gmail.com>
2024-06-09 20:41:50 +0530

    updated

--- E_Dashboard/src/App.jsx
+++ E_Dashboard/src/App.jsx
@@ -1,10 +1,23 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import Topbar from "./scenes/global/Topbar";
 
 function App() {
-  return <div className="app"></div>;
+  const [theme, colorMode] = useMode();
+
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <main className="context">
+            <Topbar />
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
 }
 
 export default App;
--- E_Dashboard/src/theme.js
+++ E_Dashboard/src/theme.js
@@ -124,4 +124,96 @@ blueAccent: {
 ),
 });
 
+// mui theme settings
+export const themeSettings= (mode)=>{
+  const colors = tokens(mode);
 
+  return{
+    palette:{
+      mode:mode,
+      ...(mode === "dark"
+        ?{
+          primary:{
+            main:colors.primary[500],
+          },
+          secondary:{
+            main:colors.greenAccent[500],
+          },
+          neutral:{
+            dark: colors.grey[700],
+            main: colors.grey[500],
+            light: colors.grey[100]
+          },
+          background:{
+            default: colors.primary[500],
+          }
+        }:{
+        primary:{
+          main:colors.primary[100],
+        },
+        secondary:{
+          main:colors.greenAccent[500],
+        },
+        neutral:{
+          dark: colors.grey[700],
+          main: colors.grey[500],
+          light: colors.grey[100]
+        },
+        background:{
+          default: "#fcfcfc",
+        },
+      }),
+    },
+    typography:{
+      fontFamily:["Source Sans Pro", "sans-serif"].join(","),
+      fontSize:12,
+      h1:{
+        fontFamily:["Source Sans Pro","sans-serif"].join(","),
+        fontSize:40,
+      },
+      h2:{
+        fontFamily:["Source Sans Pro","sans-serif"].join(","),
+        fontSize:32,
+      },
+      h3:{
+        fontFamily:["Source Sans Pro","sans-serif"].join(","),
+        fontSize:24,
+      },
+      h4:{
+        fontFamily:["Source Sans Pro","sans-serif"].join(","),
+        fontSize:20,
+      },
+      h5:{
+        fontFamily:["Source Sans Pro","sans-serif"].join(","),
+        fontSize:16,
+      },
+      h6:{
+        fontFamily:["Source Sans Pro","sans-serif"].join(","),
+        fontSize:14,
+      },
+    },
+  };
+};
+
+
+//context for color mode--to change the color
+
+export const ColorModeContext= createContext({
+  toggleColorMode:()=>{}
+});
+
+
+export const useMode=()=>{
+  const [mode,setMode]=useState("dark");
+
+  const colorMode = usememo(
+    ()=>({
+      toggleColorMode:()=>
+        setMode((prev)=>(prev==="light" ? "dark" : "light"))
+    }),
+    []
+  );
+
+  const theme =useMemo(()=>createTheme(themeSettings(mode)),[mode]);
+  return[theme,colorMode];
+};
